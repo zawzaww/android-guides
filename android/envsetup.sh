9c64826fed970ad2c322fc23a832522be1821243
Zaw Zaw <mczawzaw12@outlook.com>
2018-09-09 15:55:58 +0630

    android: Update lincense UI print for envsetup and twrp-flags
    Signed-off-by: Zaw Zaw <mczawzaw12@outlook.com>

--- android/envsetup.sh
+++ android/envsetup.sh
@@ -1,21 +1,21 @@
-###########################################################################################
-#                                                                                         #
-# Copyright(C) 2018, Zaw Zaw                                                              #
-#                                                                                         #
-# This file is free and you can distribute it and/or modify                               #
-# it under the terms of the GNU General Public License as published by                    #
-# the Free Software Foundation, either version 3 of the License, or                       #
-# any later version.                                                                      #
-#                                                                                         #        
-# This file is distributed in the hope that it will be useful,                            #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of                          #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                            #     
-# GNU General Public License for more details.                                            #
-#                                                                                         #
-# You should have received a copy of the GNU General Public License                       #
-# along with this program.  If not, see <http://www.gnu.org/licenses/>                    #
-#                                                                                         #
-###########################################################################################
+##########################################################################
+#                                                                        #
+#  Copyright(C) 2018, Zaw Zaw                                            #
+#                                                                        #
+#  This file is free and you can distribute it and/or modify             #
+#  it under the terms of the GNU General Public License as published by  #
+#  the Free Software Foundation, either version 3 of the License, or     #
+#  any later version.                                                    #
+#                                                                        #
+#  This file is distributed in the hope that it will be useful,          #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          #
+#  GNU General Public License for more details.                          #
+#                                                                        #
+#  You should have received a copy of the GNU General Public License     #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>  #
+#                                                                        #
+##########################################################################
 
 #!/usr/bin/env bash
 # A Script to automatic setup environment for android ROM development
